fbdaebf2519630e889c35b145f91aea8dba7c198
Denis Lee <denis.lee@autorei.com.br>
2013-03-22 17:54:27 -0300

    changing media directory

--- urls.py
+++ urls.py
@@ -4,39 +4,37 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^$', "agenda.views.lista"),
-    (r'^adiciona/$', "agenda.views.adiciona"),
+    (r'^$', "ncat.views.lista"),
+    (r'^adiciona/$', "ncat.views.adiciona"),
     
-    (r'^c/(?P<nr_item>\d+)/$', "agenda.views.item"),
+    (r'^c/(?P<nr_item>\d+)/$', "ncat.views.item"),
     
-    (r'^c/(?P<nr_item>\d+)/editar/$', "agenda.views.item_editar"),	# edita o post (se voce for o dono, claro)
-    (r'^c/(?P<nr_item>\d+)/comentar/(?P<nr_papai>\d+)$', "agenda.views.item_comentar"),	# comentar o post (se vc estiver logado)
-    (r'^c/(?P<nr_item>\d+)/comentarios/$', "agenda.views.item_comentarios"),	# request para ver os comentarios do post
+    (r'^c/(?P<nr_item>\d+)/editar/$', "ncat.views.item_editar"),	# edita o post (se voce for o dono, claro)
+    (r'^c/(?P<nr_item>\d+)/comentar/(?P<nr_papai>\d+)$', "ncat.views.item_comentar"),	# comentar o post (se vc estiver logado)
+    (r'^c/(?P<nr_item>\d+)/comentarios/$', "ncat.views.item_comentarios"),	# request para ver os comentarios do post
 
-    (r'^c/(?P<nr_item>\d+)/up/$', "agenda.views.vote_up"),
-    (r'^c/(?P<nr_item>\d+)/down/$', "agenda.views.vote_down"),
+    (r'^c/(?P<nr_item>\d+)/up/$', "ncat.views.vote_up"),
+    (r'^c/(?P<nr_item>\d+)/down/$', "ncat.views.vote_down"),
 
-    (r'^c/(?P<nr_item>\d+)/up_c/$', "agenda.views.vote_up_c"),
-    (r'^c/(?P<nr_item>\d+)/down_c/$', "agenda.views.vote_down_c"),
+    (r'^c/(?P<nr_item>\d+)/up_c/$', "ncat.views.vote_up_c"),
+    (r'^c/(?P<nr_item>\d+)/down_c/$', "ncat.views.vote_down_c"),
 
-    (r'^remove/(?P<nr_item>\d+)/$', "agenda.views.remove"),
+    (r'^remove/(?P<nr_item>\d+)/$', "ncat.views.remove"),
     
-    #(r'^login/', "django.contrib.auth.views.login"),
     (r'^login/', "django.contrib.auth.views.login", { 
     	"template_name": "login.html" }),
     (r'^logout/', "django.contrib.auth.views.logout_then_login", { 
     	"login_url": "/" }),    	
-#    (r'^logout/', "django.contrib.auth.views.logout_then_login", { 
-#    	"login_url": "/login/" }),    	
+
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
     
-    #registro de usuario
-    (r'^registrar/', "agenda.views.registrar"),
+    (r'^registrar/', "ncat.views.registrar"),
 
-    (r'syncdb/$', "agenda.views.syncdb"), # atualizar a pontuacao dos posts/comentarios.    
+    (r'syncdb/$', "ncat.views.syncdb"), # atualizar a pontuacao dos posts/comentarios.    
+    
     #jquery test
-    (r'^jquery/', "agenda.views.jquery"),
+    (r'^jquery/', "ncat.views.jquery"),
 )
 
 if settings.DEBUG:
